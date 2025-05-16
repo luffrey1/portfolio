c3fab9984417eabcc87ac954c40535af70272a61
luffrey1 <tu_email@ejemplo.com>
2025-05-16 17:58:36 +0200

    google search consol

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
   },
   basePath: '/portfolio', // Ruta base para GitHub Pages
   assetPrefix: '/portfolio/', // Prefijo para los assets en GitHub Pages
+  
+  // Asegurar que el archivo de verificación de Google esté disponible en la raíz
+  async rewrites() {
+    return [
+      {
+        source: '/google6111867eaad1b79b.html',
+        destination: '/portfolio/google6111867eaad1b79b.html'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig 
